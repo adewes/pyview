936fa447b9d743d17a20b14cdd59ef297362d7d9
ThePhysicist <andreas.dewes@gmail.com>
2012-07-21 13:48:51 +0300

    Update config/parameters.py

--- config/parameters.py
+++ config/parameters.py
@@ -7,3 +7,5 @@ params = dict()
 params['path'] = os.path.dirname(os.path.abspath(__file__+"/.."))
 params['directories.crystalIcons'] = '/gui/editor/data/icons/crystal/crystal_project/16x16'
 params['directories.famfamIcons'] = '/gui/editor/data/icons/famfam/icons/'
+
+#This is a comment...
